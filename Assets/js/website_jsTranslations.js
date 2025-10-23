4bb2b1af81503faa9399ff8da9ac584590561e91
Lijo Chacko <lijoc.work@gmail.com>
2025-10-24 00:21:40 +0200

    -

--- Assets/js/website_jsTranslations.js
+++
@@ -1,5 +0,0 @@
-// Minimal translations stub to prevent runtime errors from a missing translation file.
-window.websiteTranslations = window.websiteTranslations || {};
-window.websiteTranslations['en'] = window.websiteTranslations['en'] || {};
-// Add any needed translation keys below, e.g.:
-// window.websiteTranslations['en']['cookieConsentText'] = "This website uses cookies to ensure you get the best experience on our website.";
